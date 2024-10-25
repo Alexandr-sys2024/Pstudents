92cd57e08ef1c982d82adb1a75ddacd401776873
Alexandr-sys2024 <time2016slot@gmail.com>
2024-10-25 13:30:30 +0300

    add new file dz

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ import pandas as pd
 df = pd.read_csv('student_sleep_patterns.csv')
 
 
-print(df.head()) # по умолчанию выводит 5 строк сначала
+#print(df.head()) # по умолчанию выводит 5 строк сначала
+#print(df.info()) # вывод информации о файле
+print(df.describe())  #вывод статистической информации
